ab1f44bcdd8e02c24c4e748d74c4d1484bf1c546
stefSPN <111884781+stefSPN@users.noreply.github.com>
2022-08-23 16:08:50 +0200

    Update streamlit_app.py
    add line header, menu 3 lignes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfaste menu')
+streamlit.text('Omega 3 & bluberry Oatmeals')
+streamlit.text('Kale, spinach, ...')
+streamlit.text('Hard boiled ...)
