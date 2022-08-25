1ad99739790f72bc40c6511717f3b0ab812345a9
stefSPN <111884781+stefSPN@users.noreply.github.com>
2022-08-25 16:47:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,20 +43,26 @@ try:
      
 except URLError as e:
     streamlit.error()
-        
+    
+streamlit.header("The fruit load list contains:")
+#Snowflake related function
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("SELECT * from fruit_load_list")
+        return my_cur.fetchall()
+
+#Add a button to load fruit
+if streamlit.button('Get fruit load list'): 
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = my_cur.get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+
 #don't run anything past here while we troubleshoot
 streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
+    
 # Allow the end user to add a fruit to the list
 add_my_fruit  = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 #This will not work correctly
-my_cur.execute("insert into fruit_load_list values ('from strealit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
